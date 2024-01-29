99b9fb3196c527e85672801b77f1326793232bba
emiliayoffie <emiliayoffie@gmail.com>
2024-01-29 02:50:00 -0500

    refactors buttons

--- src/Components/Buttons/GroupSizesColors.tsx
+++ src/Components/Buttons/GroupSizesColors.tsx
@@ -1,13 +1,5 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import ButtonGroup from '@mui/material/ButtonGroup';
-
-// const buttons = [
-//   <Button key="one">One</Button>,
-//   <Button key="two">Two</Button>,
-//   <Button key="three">Three</Button>,
-// ];
 
 type groupSizeColorsProps = {
     buttons: JSX.Element[];
@@ -28,12 +20,6 @@ export default function GroupSizesColors({buttons}: groupSizeColorsProps) {
       <ButtonGroup variant="text" aria-label="text button group">
         {buttons}
       </ButtonGroup>
-      {/* <ButtonGroup color="secondary" aria-label="medium secondary button group">
-        {buttons}
-      </ButtonGroup>
-      <ButtonGroup size="large" aria-label="large button group">
-        {buttons}
-      </ButtonGroup> */}
     </Box>
   );
 }
