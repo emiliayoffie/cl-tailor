c0a3533230cb3999fbed6f940645903e4f0dd8aa
emiliayoffie <emiliayoffie@gmail.com>
2024-01-29 02:53:15 -0500

    updates main to include theme toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Components/App.tsx';
 import '../styles.css';
+import ToggleColorMode from './Components/ToggleColorMode.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ToggleColorMode />
   </React.StrictMode>
 );
